cabe445fbf8e26b015d9d888b14d81b6063d9972
Sam Vevang <sam.vevang@gmail.com>
2015-07-01 11:35:50 -0500

    WIP magic smelt music

--- src/blotzy.js
+++ src/blotzy.js
@@ -1,7 +1,17 @@
 $(function(){
 
+    var audioFiles = [
+        "quixote/18 Burlesque De Don Quixote, Tvw55, Ouverture.m4a",
+        "quixote/19 Burlesque De Don Quixote, Tvw55, Don Quixote Awakens.m4a",
+        "quixote/20 Burlesque De Don Quixote, Tvw55, His Attack On The Windmills.m4a",
+        "quixote/21 Burlesque De Don Quixote, Tvw55, Sighs Of Love For Princess Dulcinea.m4a",
+        "quixote/22 Burlesque De Don Quixote, Tvw55, Sancho Panza Tossed In A Blanket.m4a",
+        "quixote/23 Burlesque De Don Quixote, Tvw55, The Galloping Of Rosinante, And That Of Sancho's Mule.m4a",
+        "quixote/24 Burlesque De Don Quixote, Tvw55, Quixote Asleep.m4a",
+        "UnderTheRuglive.mp3"]
+
     $('video,audio').mediaelementplayer({
-    features: ['playpause','current','progress','duration','volume']
+    features: ['playpause','progress','current','duration','tracks','volume','fullscreen']
     });
 
 })
