525a5fd2750a378e23d6115c8e7be624866dbe2c
Sam Vevang <sam.vevang@gmail.com>
2015-07-01 10:20:41 -0500

    remove bower

--- src/blotzy.js
+++ src/blotzy.js
@@ -1,1 +1,12 @@
-var i = 3
+$(function(){
+soundManager.setup({
+  url: 'bower_components/soundmanager2/swf/',
+  flashVersion: 9, // optional: shiny features (default = 8)
+  // optional: ignore Flash where possible, use 100% HTML5 mode
+  // preferFlash: false,
+  onready: function() {
+    // Ready to use; soundManager.createSound() etc. can now be called.
+  }
+});
+
+})
