4a3ed2b5a7fbaaca25d1150d65e2623a8bb24a4e
Sam Vevang <sam.vevang@gmail.com>
2015-07-01 10:54:29 -0500

    replace sound manager with media element

--- src/blotzy.js
+++ src/blotzy.js
@@ -1,12 +1,7 @@
 $(function(){
-soundManager.setup({
-  url: 'bower_components/soundmanager2/swf/',
-  flashVersion: 9, // optional: shiny features (default = 8)
-  // optional: ignore Flash where possible, use 100% HTML5 mode
-  // preferFlash: false,
-  onready: function() {
-    // Ready to use; soundManager.createSound() etc. can now be called.
-  }
-});
+
+    $('video,audio').mediaelementplayer({
+    features: ['playpause','current','progress','duration','volume']
+    });
 
 })
