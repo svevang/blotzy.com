3da7e73c0cc998f0c5141427ae09a89ab551a688
Sam Vevang <sam.vevang@gmail.com>
2015-07-01 12:41:55 -0500

    music is playing

--- src/blotzy.js
+++ src/blotzy.js
@@ -1,17 +1,30 @@
 $(function(){
 
-    var audioFiles = [
-        "quixote/18 Burlesque De Don Quixote, Tvw55, Ouverture.m4a",
-        "quixote/19 Burlesque De Don Quixote, Tvw55, Don Quixote Awakens.m4a",
-        "quixote/20 Burlesque De Don Quixote, Tvw55, His Attack On The Windmills.m4a",
-        "quixote/21 Burlesque De Don Quixote, Tvw55, Sighs Of Love For Princess Dulcinea.m4a",
-        "quixote/22 Burlesque De Don Quixote, Tvw55, Sancho Panza Tossed In A Blanket.m4a",
-        "quixote/23 Burlesque De Don Quixote, Tvw55, The Galloping Of Rosinante, And That Of Sancho's Mule.m4a",
-        "quixote/24 Burlesque De Don Quixote, Tvw55, Quixote Asleep.m4a",
-        "UnderTheRuglive.mp3"]
+    var audioFiles = {
+        utr:   ["UnderTheRuglive.mp3"],
+        kalileh: ["kalileh.mp3"],
+        dq: [
+            "quixote/18-Burlesque-De-Don-Quixote-Tvw55-Ouverture.mp3",
+            "quixote/19-Burlesque-De-Don-Quixote-Tvw55-Don-Quixote-Awakens.mp3",
+            "quixote/20-Burlesque-De-Don-Quixote-Tvw55-His-Attack-On-The-Windmills.mp3",
+            "quixote/21-Burlesque-De-Don-Quixote-Tvw55-Sighs-Of-Love-For-Princess-Dulcinea.mp3",
+            "quixote/22-Burlesque-De-Don-Quixote-Tvw55-Sancho-Panza-Tossed-In-A-Blanket.mp3",
+            "quixote/23-Burlesque-De-Don-Quixote-Tvw55-The-Galloping-Of-Rosinante,-And-That-Of-Sancho's-Mule.mp3",
+            "quixote/24-Burlesque-De-Don-Quixote-Tvw55-Quixote-Asleep.mp3"],
+    }
 
+     $(function(){
+        _(audioFiles).chain().keys().map(function(key){
+            console.log(key);
+            _(audioFiles[key]).map(function(filename){
+                $('#'+key+' .col-xs-12').append('<audio src="magic-smelt-music/' + filename + '"></div>')
+                //$('#'+key).append()
+            })
+        })
     $('video,audio').mediaelementplayer({
     features: ['playpause','progress','current','duration','tracks','volume','fullscreen']
     });
+     })
+
 
 })
